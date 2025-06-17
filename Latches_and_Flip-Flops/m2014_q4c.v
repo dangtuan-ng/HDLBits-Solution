b5321435351c545e85599655e65b4660f1758798
dangtuan-ng <ndangtuan18@gmai.com>
2025-06-17 13:40:39 +0700

    solved problems update

--- Latches_and_Flip-Flops/m2014_q4c.v
+++ Latches_and_Flip-Flops/m2014_q4c.v
@@ -1,12 +1,10 @@
 module top_module (
     input clk,
-    input d,
-    input ar,
-    output q
-);
-    always @(posedge clk or posedge ar) begin
-        if(ar) q <= 1'b0;
+    input d, 
+    input r,   // synchronous reset
+    output q);
+    always @(posedge clk) begin
+        if(r) q <= 1'b0;
         else q <= d;
     end
 endmodule
-        
